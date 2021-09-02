8aef454fe1e438a06ddfd4ecd298a4d1f3511c4b
TTJ1974 <johntucker1974@live.com>
2021-09-02 14:37:49 -0700

    app1 update

--- app1.js
+++ app1.js
@@ -1,7 +1,22 @@
-console. log('hello bar patrons!')
+  
+// console.log('hello beer drinkers')
 
-// string
-let stringVariable = 'beer is good'
+// String
+// let stringVariable = 'wiskey is better!'
 
 // Int / number
 // let numberVariable = 8
+
+// Boolean
+// let boolearVariable = true
+
+// stringVariable = 'beer is better!'
+
+// console.log(stringVariable)
+
+let userName = prompt('Please Enter your Name: ');
+
+if(userName == 'john'){
+    document.write('Welcome wiskey lover! ' + userName);
+} else {
+    document.write(userName)
