78035b3fb5da63ea8c27885600b85a3a30a3409f
TTJ1974 <johntucker1974@live.com>
2021-09-02 22:05:42 -0700

    app2 update

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 let userAnswer = prompt('Do you like beer')
 console.log(userAnswer)
 if(userAnswer.toLowerCase() == 'yes'){
-    let url = "https://i.ibb.co/RBqQVXq/lightspeed-pilsner-can1-720x.jpg
+    let url = "https://i.ibb.co/RBqQVXq/lightspeed-pilsner-can1-720x.jpg"
     document.write('<img src="' + url + '">')
 }
