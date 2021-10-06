adea0efb9b418c131b7f0c423f6e2451653512f7
TTJ1974 <johntucker1974@live.com>
2021-10-06 15:31:55 -0700

    initial push for app1

---
+++ app1.js
@@ -0,0 +1,27 @@
+// / console.log("we made it")
+
+// let userName = "John";
+// let userlastName = "Tucker"
+
+// console.log(userName + userLastName)
+
+// console.log(typeof(userName))
+
+// let userAge = 47;
+// console.log(typeof(isTeacher));
+
+// let fiftyFive = "55";
+// console.log(The variable fiftyFive is a ' + typeof(fiftyFive))
+// ;
+// Let userName = prompt('Please enter your name!');
+// console.log(userName);
+
+// document.write(userName);let userName = prompt('Please enter your name!');
+console.log(userName);
+
+if(userName == 'Roger'){
+    document.write('<h2>Welcome Lord ' + userName + '</h2>');
+} else {
+    document.write('<h2>Welcome ' + userName + '</h2>');
+}
+
