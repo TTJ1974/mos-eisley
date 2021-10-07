39f4c4d84e4371a7b21942115defea0c3e476667
TTJ1974 <johntucker1974@live.com>
2021-10-07 11:11:40 -0700

    update

--- app1.js
+++ app1.js
@@ -1,25 +1,9 @@
-// / console.log("we made it")
-
-// let userName = "John";
-// let userlastName = "Tucker"
-
-// console.log(userName + userLastName)
-
-// console.log(typeof(userName))
-
-// let userAge = 47;
-// console.log(typeof(isTeacher));
-
-// let fiftyFive = "55";
-// console.log(The variable fiftyFive is a ' + typeof(fiftyFive))
-// ;
-
 let userName = prompt('Please enter your name!');
-console.log(userName);
+    console.log(userName);
 
 if(userName == 'john'){
     document.write('<h2>Welcome Big Sexy! ' + userName + '</h2>');
-} else {
+    } else {
     document.write('<h2>Welcome ' + userName + '</h2>');
-}
+    }
 
