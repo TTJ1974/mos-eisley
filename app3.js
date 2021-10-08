f0dffa2d3f54ac395385bf31cf269ccbdb7ea4ff
TTJ1974 <johntucker1974@live.com>
2021-10-08 05:43:14 -0700

    app3 upload

--- app3.js
+++ app3.js
@@ -1,33 +1,30 @@
-// validate that the user enters a number > 0 and <= 100
 
-// check if the user guess is higher or lower than the correct answer
+function guessingGame(){
+    // prompt user to enter a number
+    
+    let userAnswer = prompt('would you like to play a game?');
+    let correctAnswer = yes; 
+    let userAnswer = prompt('Please Enter a number between 1-100');
+    let correctAnswer = 71; // maybe we can randomize this later
+    let attempts = 8;
 
-// limit how many the user can guess
+    for(let i = 0; i < attempts; i++){
+        console.log(i)
+        while(userAnswer < 1|| userAnswer > 100){
+            userAnswer = prompt('Try Again: Please Enter a number between 1-100');
+        }
+        if(userAnswer == correctAnswer){
+            alert('DING DING DING.  You Got it right');
+            break;
+        }else if (userAnswer > correctAnswer && i != 7){
+            userAnswer = prompt('To High. Try Again 1-100');
+        }else if(userAnswer < correctAnswer && i != 7){
+            userAnswer = prompt('To Low. Try Again 1-100');
+        } else{
+            alert('Sorry, you Lost!')
+        }
 
 
-function userGuessingGame(correctAnswer){
-  let wannaPlay = prompt('Do you want to play a game? (yes or no)');
-  console.log(wannaPlay)
-  while (wannaPlay.toLowerCase() == 'yes'){
-      let numberOfAttempts = 8;
-      for(let i = 1; i <= numberOfAttempts; i++){
-          userAnswer = prompt("Guess a number between 1 and 100");
-  
-          while((userAnswer < 1) || (userAnswer > 100)){
-              userAnswer = prompt("Guess a number between 1 and 100");
-          }
-          if(userAnswer == correctAnswer){
-              alert('You got it right. You have ' + (numberOfAttempts - i) + ' attempts left');
-              break;
-          } else if(userAnswer < correctAnswer){
-              alert('Your answer is to low.  Try Again! You have ' + (numberOfAttempts - i) + ' attempts left');  
-          } else if (userAnswer > correctAnswer){
-              alert('Your answer is to high.  Try Again! You have ' + (numberOfAttempts - i) + ' attempts left');
-          }
-      }
-      wannaPlay = prompt('Do you want to play a game? (yes or no)');
-      if (wannaPlay == 'yes'){
-          correctAnswer = randomNumberGenerator();
-      }
-  }
-}
+       
+            }
+    }
