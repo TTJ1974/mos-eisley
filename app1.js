61e8bc918786e58c60b9f166152345275d318b59
TTJ1974 <johntucker1974@live.com>
2021-10-08 05:19:05 -0700

    app1 consolodation

--- app1.js
+++ app1.js
@@ -11,3 +11,14 @@ if(userName == 'john'){
     return userName;
 }
 
+
+function beerrules(){
+    let userAnswer = prompt('Do you like beer');
+       // console.log(userAnswer)
+       
+    if(userAnswer.toLowerCase() == 'yes'){
+        let url = "https://i.ibb.co/RBqQVXq/ lightspeed-pilsner-can1-720x.jpg";
+       document.write('<img src="' + url + '">');
+   }
+   
+     }
